bbc3273bc5ef81e43439767eb8e9b3e4f4c519d8
Martin John M <johnmmartin74@gmail.com>
2025-08-05 23:56:29 +0200

    created golang service for pushing 5000 car events to a redis pub sub on every second

--- telematics-pusher-golang/main.go
+++ telematics-pusher-golang/main.go
@@ -2,64 +2,38 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"math/rand"
+	"log"
+	"sync"
+	"telematics-pusher-golang/manager"
 	"time"
-
-	"github.com/redis/go-redis/v9"
 )
 
-var ctx = context.Background()
-
-type TelematicsData struct {
-	CarID    int     `json:"car_id"`
-	Speed    float64 `json:"speed"`
-	Lat      float64 `json:"lat"`
-	Lon      float64 `json:"lon"`
-	Timestamp int64  `json:"timestamp"`
-}
-
-func generateCarData(carID int) TelematicsData {
-	return TelematicsData{
-		CarID:    carID,
-		Speed:    rand.Float64()*150.0 + 1,
-		Lat:      40.0 + rand.Float64()*20.0,
-		Lon:      5.0 + rand.Float64()*20.0,
-		Timestamp: time.Now().Unix(),
-	}
-}
-
-func startPushing(client *redis.Client, carID int) {
-	ticker := time.NewTicker(1 * time.Second)
-	defer ticker.Stop()
-
-	for range ticker.C {
-		data := generateCarData(carID)
-		jsonData, err := json.Marshal(data)
-		if err != nil {
-			fmt.Printf("Car %d: JSON marshal error: %v\n", carID, err)
-			continue
-		}
-
-		err = client.LPush(ctx, "car_telematics", jsonData).Err()
-		if err != nil {
-			fmt.Printf("Car %d: Redis push error: %v\n", carID, err)
-		}
-	}
-}
-
 func main() {
-	rand.Seed(time.Now().UnixNano())
+	log.Println("Starting Car Telematics Pusher Service for 5000 cars...")
 
-	client := redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
-	})
+	carManager := manager.NewCarManager("localhost:6379", "car:telemetry")
+	
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	carCount := 5000
-	for carID := 1; carID <= carCount; carID++ {
-		go startPushing(client, carID)
+	var wg sync.WaitGroup
+	totalCars := 5000
+
+	for i := 1; i <= totalCars; i++ {
+		wg.Add(1)
+		carID := fmt.Sprintf("car-%d", i)
+
+		go func(id string) {
+			defer wg.Done()
+			carManager.SimulateAndPushWithContext(ctx, carID)
+		}(carID)
 	}
 
-	select {} // block forever
+	// Run for 10 seconds then stop all goroutines
+	time.Sleep(100 * time.Second)
+	cancel()
+	wg.Wait()
+
+	log.Println("Stopped all car telemetry pushes")
 }
