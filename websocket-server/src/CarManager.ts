d178cfc0fa7f4b8888a77442eb26b93b33b4e6ca
Martin John M <johnmmartin74@gmail.com>
2025-09-13 21:42:14 +0200

    put types in a common folder

--- websocket-server/src/CarManager.ts
+++ websocket-server/src/CarManager.ts
@@ -4,28 +4,7 @@ import { SubsciptionManager } from "./SubscriptionManager"
 import { createClient, RedisClientType } from "redis";
 import { UserManager } from "./UserManager";
 import { MaxPriorityQueue } from "./heap/MaxPriorityQueue";
-
-export interface CarData {
-  type: string;
-  id: string;
-  city: string;
-  speed: number;
-  latitude: number;
-  longitude: number;
-  fuel_level: number;
-  direction: number;
-  status: string;
-  timestamp: number;
-}
-
-type SpeedBin = { range: string; count: number };
-
-
-export interface AnalyticsData {
-    type : string, 
-    top_ten_cars : [string, CarData][]
-    speed_histogram : SpeedBin[]
-}
+import { CarData, SpeedBin } from "./types/car";
 
 export class CarManager {
     private static instance : CarManager;
