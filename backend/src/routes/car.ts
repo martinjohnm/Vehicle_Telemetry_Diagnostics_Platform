1c9a3f9c6788f2eac6d5affd3dcbf24d7510fd63
Martin John M <johnmmartin74@gmail.com>
2025-09-19 06:49:53 +0200

    Created backend apis for different timeframes

--- backend/src/routes/car.ts
+++ backend/src/routes/car.ts
@@ -48,3 +48,107 @@ carRouter.get("/1m/:id", async (req : Request,res : Response) => {
     }
     
 })
+
+carRouter.get("/5m/:id", async (req : Request,res : Response) => {
+
+
+    try {
+        const { id } = req.params;
+        // SELECT bucket, avg_speed FROM car_speed_1m where id='CAR-123' and bucket >= NOW() - interval '2 days';
+        let query = `SELECT * FROM car_speed_5m WHERE bucket >= $1 AND bucket <= $2 and id='${id}'`;
+
+        const result = await pgClient.query(query, [new Date("2025-09-15T00:00:00Z"), new Date()])
+
+        return res.json({
+            result : result.rows
+        })
+        
+    } catch(e) {
+
+    }
+    
+})
+
+
+carRouter.get("/10m/:id", async (req : Request,res : Response) => {
+
+
+    try {
+        const { id } = req.params;
+        // SELECT bucket, avg_speed FROM car_speed_1m where id='CAR-123' and bucket >= NOW() - interval '2 days';
+        let query = `SELECT * FROM car_speed_10m WHERE bucket >= $1 AND bucket <= $2 and id='${id}'`;
+
+        const result = await pgClient.query(query, [new Date("2025-09-15T00:00:00Z"), new Date()])
+
+        return res.json({
+            result : result.rows
+        })
+        
+    } catch(e) {
+
+    }
+    
+})
+
+
+carRouter.get("/15m/:id", async (req : Request,res : Response) => {
+
+
+    try {
+        const { id } = req.params;
+        // SELECT bucket, avg_speed FROM car_speed_1m where id='CAR-123' and bucket >= NOW() - interval '2 days';
+        let query = `SELECT * FROM car_speed_15m WHERE bucket >= $1 AND bucket <= $2 and id='${id}'`;
+
+        const result = await pgClient.query(query, [new Date("2025-09-15T00:00:00Z"), new Date()])
+
+        return res.json({
+            result : result.rows
+        })
+        
+    } catch(e) {
+
+    }
+    
+})
+
+
+carRouter.get("/30m/:id", async (req : Request,res : Response) => {
+
+
+    try {
+        const { id } = req.params;
+        // SELECT bucket, avg_speed FROM car_speed_1m where id='CAR-123' and bucket >= NOW() - interval '2 days';
+        let query = `SELECT * FROM car_speed_30m WHERE bucket >= $1 AND bucket <= $2 and id='${id}'`;
+
+        const result = await pgClient.query(query, [new Date("2025-09-15T00:00:00Z"), new Date()])
+
+        return res.json({
+            result : result.rows
+        })
+        
+    } catch(e) {
+
+    }
+    
+})
+
+
+carRouter.get("/1h/:id", async (req : Request,res : Response) => {
+
+
+    try {
+        const { id } = req.params;
+        // SELECT bucket, avg_speed FROM car_speed_1m where id='CAR-123' and bucket >= NOW() - interval '2 days';
+        let query = `SELECT * FROM car_speed_1h WHERE bucket >= $1 AND bucket <= $2 and id='${id}'`;
+
+        const result = await pgClient.query(query, [new Date("2025-09-15T00:00:00Z"), new Date()])
+
+        return res.json({
+            result : result.rows
+        })
+        
+    } catch(e) {
+
+    }
+    
+})
