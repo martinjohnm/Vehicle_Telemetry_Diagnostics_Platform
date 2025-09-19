70b1803dacc2a6b109be6d00c16e6662caa3f43a
Martin John M <johnmmartin74@gmail.com>
2025-09-19 08:30:10 +0200

    Added cors origin for frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,18 @@
 
 
 import express from "express"
+import cors from "cors";
 
 import { carRouter } from "./routes/car"
 
 
 const app = express()
-
+// Allow requests from React dev server (port 5173)
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true, // if you want to send cookies
+}));
 app.use(express.json())
 
 app.use("/api/v1/car", carRouter)
